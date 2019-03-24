50929c07448ea8c04d34a1e515d3920332a6e27a
Victor Medina <vittico@gmail.com>
2019-03-24 14:21:30 -0400

    gitignore added/aws subnet/default vpc

--- simple/main.tf
+++ simple/main.tf
@@ -24,4 +24,36 @@ resource "aws_route" "default" {
 }
 
 
+resource "aws_subnet" "default-small"{
+  vpc_id  = "${aws_vpc.main-vpc.id}"
+  cidr_block = "10.254.0.0/24"
+  map_public_ip_on_launch = true
 
+  tags{
+    Name="default-small"
+  }
+}
+
+
+resource "aws_default_security_group" "default" {
+  vpc_id = "${aws_vpc.main-vpc.id}"
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags{
+    Name="Default SG - Managed by Terraform"
+  }
+
+}
